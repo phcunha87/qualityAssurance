857740b00bb622e8a796648c7d48e1d2876564fc
Phelipe Cunha <santosphelipee@gmail.com>
2020-07-05 01:24:44 -0300

    deletando as fotos

--- src/main/java/br/com/qa/testes/PesquisarTeste.java
+++ src/main/java/br/com/qa/testes/PesquisarTeste.java
@@ -18,7 +18,7 @@ public class PesquisarTeste extends BaseTest{
 		pt.url("http://www.google.com.br");
 		pt.escrevePesquisa("Testes Automatizados");
 		pt.submeterPesquisa();
-		assertEquals("Com uuma infraestrutura simples, entregue Aplicações para Mercado mais Rápido e Barato. Descobra como a IA acelera a Identificação da Raiz de Problemas com IBM. Descubra mais. Implementação estratégica. Indústria Focada Solution. Metodologias avançadas.‎", pt.validaPesquisa());
+		assertEquals("Com uma infraestrutura simples, entregue Aplicações para Mercado mais Rápido e Barato. Descobra como a IA acelera a Identificação da Raiz de Problemas com IBM. Descubra mais. Implementação estratégica. Indústria Focada Solution. Metodologias avançadas.‎", pt.validaPesquisa());
 		
 	}
 
